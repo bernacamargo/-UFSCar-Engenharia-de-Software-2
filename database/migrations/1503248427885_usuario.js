f2d71e0ccea1d81bc284fd9b3d75fee43fc4e955
Bernardo Camargo <bernardopcamargo@gmail.com>
2020-11-07 12:21:06 -0300

    core: add migration usuarios

--- database/migrations/1503248427885_usuario.js
+++ database/migrations/1503248427885_usuario.js
@@ -7,15 +7,18 @@ class UserSchema extends Schema {
   up () {
     this.create('usuarios', (table) => {
       table.increments()
+      table.string('nome', 254).notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('senha', 60).notNullable()
       table.integer('nivel_acesso').notNullable()
+      table.string('celular', 150)
+      table.string('cpf', 20).notNullable()
       table.timestamps()
     })
   }
 
   down () {
-    this.drop('users')
+    this.drop('usuarios')
   }
 }
 
